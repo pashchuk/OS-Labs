8d30509a75b3441946a952a87bcbefaea77a58ee
Eduard <mamaxropela@gmail.com>
2014-06-04 01:25:28 +0300

    add main structures

--- FileSystem/FileSystem/Program.cs
+++ FileSystem/FileSystem/Program.cs
@@ -10,6 +10,28 @@ namespace FileSystem
     {
         static void Main(string[] args)
         {
+            
         }
+    }
+    /// <summary>
+    /// Index-node for simple file
+    /// </summary>
+    struct IFNode
+    {
+        int index
+    }
+    /// <summary>
+    /// Index-node for directory
+    /// </summary>
+    struct IDNode
+    {
+        
+    }
+    /// <summary>
+    /// Struct for block with data
+    /// </summary>
+    struct DataBlock
+    {
+        
     }
 }
